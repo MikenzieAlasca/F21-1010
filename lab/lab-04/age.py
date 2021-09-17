e45f742f51cabe5d2992bf51619135cdb6ba802c
Philip Schlump <pschlump@gmail.com>
2021-09-17 11:24:43 -0600

    New pdf

--- lab/lab-04/age.py
+++ lab/lab-04/age.py
@@ -4,16 +4,16 @@ age = int(age_str)
 
 ticket_price = 0
 if age >= 0 and age <= 4:
-	ticket_price = 0
+    ticket_price = 0
 elif age >= 5 and age <= 12:
-	ticket_price = 38
+    ticket_price = 38
 elif age >= 13 and age <= 17:
-	ticket_price = 48
+    ticket_price = 48
     # Add Code
     # Add Code
     # Add Code
     # Add Code
 else:
-	print ( "Impossile Age" )
+    print ( "Impossile Age" )
 
 print ( "Price = {}".format(ticket_price) ) 
